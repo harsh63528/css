bb5863bbdf8ae3c198c5cc7f062e528685d62e11
harsh singh <vduharsh123@gmail.com>
2026-05-31 22:08:29 +0530

    waste folder

---
+++ waste/script.js
@@ -0,0 +1,34 @@
+// class object{
+//     constructor(object,life){
+//         this.object=object;
+//         this.life=life;
+//     }
+//     print(){
+//         console.log(object,life)
+//     }
+// }
+
+
+// class living extends object {
+//     constructor(object,life){
+//         super(object,life)
+//     }
+// }
+
+// const human= new object('human','living')
+// human.print();
+
+
+// closure
+// function check(num1,num2){
+//     let a=num1;
+//     let b=num=2;
+
+//     return function add(){
+//         a+b;   
+//     }
+// }
+
+// let a1=check(12)
+// console.dir(a1)
+
