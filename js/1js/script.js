ac5017cafcea85c9ae32fc4fbed77188fc850708
harsh singh <vduharsh123@gmail.com>
2026-06-07 21:34:26 +0530

    dsa 1 st day

--- js/1js/script.js
+++ js/1js/script.js
@@ -1,25 +1,42 @@
-class calculate{
-    constructor(val1,val2){
-        this.value1=val1;
-        this.value2=val2;
+// 
+
+// Q1 sum of all natural number
+
+// function sum(num){
+//     let result=0;
+//     return function (){
+        
+//         for(let i=1;i<=num;i++){
+//       result+=i;
+//     }
+//      return result
+//     }
+
+//     return result
+// }
+
+// const v1=sum(5)
+// const v2=sum(10)
+// console.log(`v1 :${v1()}`)
+// console.dir(`v1 second time :${v1()}`)
+
+// console.log(`v2 :${v2()}`)
+// console.dir(`v2 second time :${v2()}`)
+
+// sum of digit of number for eg 635284
+
+function ranSum(num){
+    let result=0;
+    let str=num.toString()
+    return function(){
+        const value=str.split("")
+        for(let i=0;i<= value.length;i++){
+            console.log(`running ${i}`)
+            result+=Number(value[i])
+        }
+         return result
     }
-
-    addition(){
-        let add= this.value1 + this.value2;
-        console.dir(add)
-    }
-
-    substract(){
-        let sub=this.value1-this.value2
-        console.log(`sub: ${sub}`)
-    }
-
-
 }
 
-const v1= new calculate(5,10)
-const v2=new calculate(10,16)
-v1.addition();
-v1.substract();
-v2.addition();
-v2.substract();
+const value1=ranSum(63528)
+console.log(value1())
