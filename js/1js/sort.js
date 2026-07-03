b3e3ce6a788e1800be80da4d9943fa66e4094c13
harsh singh <vduharsh123@gmail.com>
2026-07-03 19:57:13 +0530

    3 rd atempt in selection sort

--- js/1js/sort.js
+++ js/1js/sort.js
@@ -28,24 +28,32 @@
 
 // step 1: start
 // step 2:create a function
-function selection(arr=[]){
+function selection(arr){
 
 // step 3:create variable for storing selected element index in array
 let selectIndex;
 // step 4:create loop for checking if the all elements are looped
 for(let i=0;i < arr.length;i++){
     selectIndex=i;
-    for (let j=i+1;j<arr.length;j++){
-        if(arr[j]<arr[selectIndex]){
-            console.log(`${arr[j] is smaller than ${arr[selectIndex]}`)
+    // step 5:create subloop for checking if the selected elemet is smaller than everyone present in loop or not
+    // step 6:if the selected element is not smaller than compare element then take that elemnt index else do nothing
+    // step 7:do setep 6 for all the element 
+    for (let j=i+1;j>arr.length;j++){
+        if(arr[j]>arr[selectIndex]){
+            console.log(`${arr[j]} is smaller than ${arr[selectIndex]}`)
             selectIndex=j;
         }
     }
-    arr[i]
+if(selectIndex !== i){
+    let temp=arr[i];
+    arr[selectIndex]=arr[i];
+    arr[i]=temp
+}
 
 }
-// step 5:create subloop for checking if the selected elemet is smaller than everyone present in loop or not
-// step 6:if the selected element is not smaller than compare element then take that elemnt index else do nothing
-// step 7:do setep 6 for all the element 
+return arr
 // step 8: exist subloop check if all the element arre check if checked then exist loop 
+
 }
+
+console.log(selection([6,3,5,3,2,4]))
