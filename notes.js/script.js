db40ee92b8d4e18a512f511b1ff722f34073c368
harsh singh <vduharsh123@gmail.com>
2026-07-07 21:04:26 +0530

    rewriting logic from scratch

--- notes.js/script.js
+++ notes.js/script.js
@@ -1,20 +1,47 @@
-const btn=document.getElementById('btn')
+// dom
 const task=document.getElementById('task')
+const btn=document.getElementById('btn')
 
-btn.addEventListener('click',(e)=>{
-    alert('i am working')
-     const info={
-        inp: e.target.value,
-        date:date.getDate
+// tasks 
+const tasks=[];
+
+// function for accessing the value
+function data(value){
+    // creating variable for storing value
+    let info;
+    return function(){
+
+        // check wheteher the input task is empty or not if empty then it alert them
+        // if it is not empty
+        if(value !== false){
+
+        // storing the value in variable info [format:task,date,tag(task is completed or not),time]
+        info={
+            task:value,
+            date:date.getDate,
+            tag:false
+        }
+    
+        return info
     }
-    console.log(info)
-    console.dir(Date)
-})
+
+    // if it is empty
+    else{
+        alert('task is empty')
+    }
+    }
+}
 
 
-// function add(info){
-//     const info={
-//         inp: info,
-//         date:date.getDate
-//     }
-// }
+// for adding the value in the tasks array   note:it is a prototype work needed
+function add(value){
+    tasks.push(value)
+}
+
+// function for reloading the task or injecting html tag in the html
+function injection(arr){
+    // looping array for each element
+    arr.array.forEach(element => {
+        let 
+    });
+}
